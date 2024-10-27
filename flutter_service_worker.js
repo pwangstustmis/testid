943c03dcf2ba92f4d1c3fd6531d5596a0cc2ef3e
pwangstustmis <37906966+pwangstustmis@users.noreply.github.com>
2024-10-27 20:00:23 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "57cf973e9f8ff7d952c7929553e782c1",
-"/": "57cf973e9f8ff7d952c7929553e782c1",
+"index.html": "f2b735a8ba783f481205e4b9ca996783",
+"/": "f2b735a8ba783f481205e4b9ca996783",
 "main.dart.js": "a69d54a183b4aca7f6d0c7f9a379d91e",
 "manifest.json": "8034f5204c4b198ec2c7512469812d4e",
 "version.json": "dacd2e8cf653dc3f1c00d5d7bb84f814"};
